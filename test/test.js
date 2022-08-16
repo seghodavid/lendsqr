a9dfa2f0dd57cc32e8838763e25f63be991818f5
seghodavid <56871763+seghodavid@users.noreply.github.com>
2022-08-16 16:24:27 +0100

    Test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const app = require('../index')
 const request = require('supertest')
 
 describe('GET /' , () => {
-    test("Responds with Hello from Lendsqr", (done) => {
+    it("Responds with Hello from Lendsqr", (done) => {
      request(app).get("/").expect("Hello from Lendsqr", done);
     });
 })
